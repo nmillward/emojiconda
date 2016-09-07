4cc08d431de33b992bb089cca670b20ee8548469
Nick Millward <nicholasmillward@gmail.com>
2016-09-07 13:55:14 -0400

    initial commit

---
+++ android/src/com/nickmillward/snake/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nickmillward.snake;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nickmillward.snake.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
---
+++ desktop/src/com/nickmillward/snake/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nickmillward.snake.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nickmillward.snake.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
