54b1c4651cdaf75111b8a3ec4b98523982afdd5e
Nick Millward <nicholasmillward@gmail.com>
2016-09-13 09:24:35 -0400

    setup point class

---
+++ core/src/com/nickmillward/snake/entities/Point.java
@@ -0,0 +1,35 @@
+package com.nickmillward.snake.entities;
+
+/**
+ * Created by nmillward on 9/13/16.
+ */
+public class Point {
+
+    private int x, y;
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
--- core/src/com/nickmillward/snake/entities/Points.java
+++
@@ -1,7 +0,0 @@
-package com.nickmillward.snake.entities;
-
-/**
- * Created by nmillward on 9/13/16.
- */
-public class Points {
-}
