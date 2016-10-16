8708823d69aa2659f46e2f54d4406589a8c64a41
Nick Millward <nicholasmillward@gmail.com>
2016-10-16 15:19:42 -0400

    update snakeHud font usage

--- core/src/com/nickmillward/snake/overlays/SnakeHUD.java
+++ core/src/com/nickmillward/snake/overlays/SnakeHUD.java
@@ -1,11 +1,13 @@
 package com.nickmillward.snake.overlays;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.nickmillward.snake.utils.Constants;
+import com.nickmillward.snake.utils.Utils;
 
 /**
  * Created by nmillward on 9/26/16.
@@ -18,8 +20,7 @@ public class SnakeHUD {
 
     public SnakeHUD() {
         this.viewport = new ExtendViewport(Constants.SNAKE_HUD_VIEWPORT_SIZE, Constants.SNAKE_HUD_VIEWPORT_SIZE);
-        font = new BitmapFont();
-        font.getData().setScale(1);
+        font = Utils.generateFreeTypeFont(Constants.FONT_FISHFONT, 24, Color.WHITE);
         shapeRenderer = new ShapeRenderer();
     }
 
