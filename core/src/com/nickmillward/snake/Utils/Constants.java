8edb04aa0dba264203b52e8aa80981c3ed6b8e73
Nick Millward <nicholasmillward@gmail.com>
2016-10-16 12:46:05 -0400

    increase emoji size

--- core/src/com/nickmillward/snake/Utils/Constants.java
+++ core/src/com/nickmillward/snake/Utils/Constants.java
@@ -17,7 +17,7 @@ public class Constants {
 
     // SNAKE
 //    public static final float SNAKE_SEGMENT_DEFAULT_SIZE = WORLD_SIZE * 0.05f;
-    public static final float SNAKE_SEGMENT_DEFAULT_SIZE = 40;
+    public static final float SNAKE_SEGMENT_DEFAULT_SIZE = 50;
     public static final Vector2 SNAKE_SEGMENT_CENTER = new Vector2(SNAKE_SEGMENT_DEFAULT_SIZE / 2, SNAKE_SEGMENT_DEFAULT_SIZE / 2);
     public static final int EASY_SNAKE_VELOCITY = 10;
     public static final int MEDIUM_SNAKE_VELOCITY = 5;
@@ -41,7 +41,7 @@ public class Constants {
 
     // FOOD
 //    public static final float FOOD_DEFAULT_SIZE = WORLD_SIZE * 0.05f;
-    public static final float FOOD_DEFAULT_SIZE = 32;
+    public static final float FOOD_DEFAULT_SIZE = 40;
     public static final Vector2 FOOD_CENTER = new Vector2(FOOD_DEFAULT_SIZE / 2, FOOD_DEFAULT_SIZE / 2);
     public static final int FOOD_BASE_POINT_VALUE = 5;
 
